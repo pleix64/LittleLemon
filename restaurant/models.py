045e2e6e61c46830927f4d2ab68b01721a70a430
Yu Chen <chenyu.bnu@gmail.com>
2024-01-23 21:53:43 -0800

    models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.PositiveSmallIntegerField()
+    booking_date = models.DateTimeField()
+    
+    def __str__(self) -> str:
+        return self.name + ", part of " + str(self.no_of_guests)
+    
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.PositiveSmallIntegerField()
+    
+    def __str__(self) -> str:
+        return self.title
